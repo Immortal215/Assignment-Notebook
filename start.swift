3cb6e3e0225f622ade7f7982a2008791bb827aaa
Sharul Shah <sharulshah@icloud.com>
2024-05-01 14:53:33 -0500

    Update start.swift

--- start.swift
+++ start.swift
@@ -7,9 +7,20 @@ struct Start: View {
             Button {
                 starter = false  
             } label : {
-                Text("The Drawing Board")
-                    .font(.custom("", fixedSize: 100))
-                    .foregroundStyle(.white)
+                ZStack {
+                    Image(systemName: "square.and.pencil")
+                        .resizable()
+                        .frame(width:200, height:200)
+                        .rotationEffect(.degrees(starter ? 0.0 : 15.0))
+                        .animation(.bouncy(duration: 1, extraBounce: 0.3))
+                    
+                        Text("The Drawing Board")
+                            .font(.custom("", fixedSize: 100))
+                            .foregroundStyle(.white)
+                            .shadow(color: .gray, radius: 5, x: 0.0, y: 0.0)
+                      
+                   
+                }
                 
             }
             
@@ -17,26 +28,28 @@ struct Start: View {
                 .frame(width: starter ? 0 : .infinity)
             NavigationLink(destination: Homepage()) { 
                 
-             
-                    RoundedRectangle(cornerRadius: 20)
-                        .foregroundStyle(.blue)
-                        .opacity(0.3)
-                      
-                          .offset(x: 0, y: starter ? -100 : 0)
-                  
-                          .animation(.bouncy(duration: 1, extraBounce: 0.1))
-                          .overlay(
-                            Text("Start Planning")
-                                .font(.custom("", fixedSize: 50))
-                                .foregroundStyle(.white)
-                                .frame(width: starter ? 0 : 500, height: starter ? 0 : 100, alignment:.center)
-                                .offset(x: 0, y: starter ? -100 : 0)
-                                 .animation(.bouncy(duration: 1, extraBounce: 0.1))
-                          )   
-                    
+                
+                RoundedRectangle(cornerRadius: 20)
+                    .foregroundStyle(.blue)
+                    .opacity(0.3)
+                
+                    .offset(x: 0, y: starter ? -100 : 0)
+                
+                    .animation(.bouncy(duration: 1, extraBounce: 0.1))
+                    .overlay(
+                        Text("Start Planning")
+                            .font(.custom("", fixedSize: 50))
+                            .foregroundStyle(.white)
+                            .frame(width: starter ? 0 : 500, height: starter ? 0 : 100, alignment:.center)
+                            .offset(x: 0, y: starter ? -100 : 0)
+                            .animation(.bouncy(duration: 1, extraBounce: 0.1))
+                    )   
+                
                 
             }
             .frame(width: starter ? 0 : 500, height: starter ? 0 : 100, alignment:.center)
+            .padding(starter ? 0 : 20)
+
             
             
             
@@ -45,7 +58,7 @@ struct Start: View {
             
             
         }
-     
+        
         
     }
 }
