d0fe56e063068a6abedc3d164b75449c07f41317
Immortal215 <sharulshah@icloud.com>
2024-04-30 19:41:59 -0500

    Update start.swift
    Minor Ui change

--- start.swift
+++ start.swift
@@ -1,16 +1,35 @@
 import SwiftUI
 
 struct Start: View {
+    @State var starter = true 
     var body: some View {
         NavigationStack {
-            Text("The Drawing Board")
-                .font(Font.custom("SF Mono", fixedSize: 100))
-            Divider()
-            NavigationLink(destination: Homepage()) { 
-                Text("Start")
-                    .font(.largeTitle)
+            Button {
+               starter = false  
+            } label : {
+                Text("The Drawing Board")
+                    .font(.custom("", fixedSize: 100))
+                    .foregroundStyle(.white)
             }
-        }
+                
+                    Divider()
+                    NavigationLink(destination: Homepage()) { 
+                        Text("Start Planning")
+                            .font(.custom("", fixedSize: 50))
+                            .frame(width: starter ? 0 : .infinity, height: starter ? 0 : 100, alignment:.center)
+                        
+                    
+                
+            }
+                    .frame(width: starter ? 0 : .infinity, height: starter ? 0 : 100, alignment:.center)
+            
+            
+                
+                
+                    
+            
+            }
+        
         
         
     }
