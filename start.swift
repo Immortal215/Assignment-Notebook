1bd921a7ef7dcc7fbd1f2b3a1278ff0dfb3d03fb
Sharul Shah <sharulshah@icloud.com>
2024-05-01 11:17:06 -0500

    Update start.swift
    Changed some more Ui

--- start.swift
+++ start.swift
@@ -5,32 +5,47 @@ struct Start: View {
     var body: some View {
         NavigationStack {
             Button {
-               starter = false  
+                starter = false  
             } label : {
                 Text("The Drawing Board")
                     .font(.custom("", fixedSize: 100))
                     .foregroundStyle(.white)
-            }
                 
-                    Divider()
-                    NavigationLink(destination: Homepage()) { 
-                        Text("Start Planning")
-                            .font(.custom("", fixedSize: 50))
-                            .frame(width: starter ? 0 : .infinity, height: starter ? 0 : 100, alignment:.center)
-                        
+            }
+            
+            Divider()
+                .frame(width: starter ? 0 : .infinity)
+            NavigationLink(destination: Homepage()) { 
+                
+             
+                    RoundedRectangle(cornerRadius: 20)
+                        .foregroundStyle(.blue)
+                        .opacity(0.3)
+                      
+                          .offset(x: 0, y: starter ? -100 : 0)
+                  
+                          .animation(.bouncy(duration: 1, extraBounce: 0.1))
+                          .overlay(
+                            Text("Start Planning")
+                                .font(.custom("", fixedSize: 50))
+                                .foregroundStyle(.white)
+                                .frame(width: starter ? 0 : 500, height: starter ? 0 : 100, alignment:.center)
+                                .offset(x: 0, y: starter ? -100 : 0)
+                                 .animation(.bouncy(duration: 1, extraBounce: 0.1))
+                          )   
                     
                 
             }
-                    .frame(width: starter ? 0 : .infinity, height: starter ? 0 : 100, alignment:.center)
+            .frame(width: starter ? 0 : 500, height: starter ? 0 : 100, alignment:.center)
             
             
-                
-                
-                    
             
-            }
-        
-        
+            
+            
+            
+            
+        }
+     
         
     }
 }
