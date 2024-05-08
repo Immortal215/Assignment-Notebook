cb93f4fc8b9732334147d46649f259a7ed04c60b
Sharul Shah <sharulshah@icloud.com>
2024-05-08 14:59:12 -0500

    Update Homepage.swift

--- Homepage.swift
+++ Homepage.swift
@@ -1,14 +1,14 @@
 import SwiftUI
 
 struct Homepage: View {
-    @State var settings = false
+   
     var body: some View {
         TabView(selection: .constant(1),
                 content:  {
             Notebook().tabItem {Text("Assignment Notebook")}.tag(1)
-            Settinger().tabItem {Text("Settings")}.tag(2).onTapGesture {
-               settings = true 
-            }
+            Settinger().tabItem {Text("Settings")}.tag(2)
+            Text("WIP").tabItem {Text("WIP")}.tag(3)
+                        Text("WIP").tabItem {Text("WIP")}.tag(4)
         })
     }
 }
