14a0c4f575fd3e5341cebcd3135e7047c1cd70c7
Sharul Shah <sharulshah@icloud.com>
2024-05-09 11:13:24 -0500

    Update Homepage.swift

--- Homepage.swift
+++ Homepage.swift
@@ -3,12 +3,6 @@ import SwiftUI
 struct Homepage: View {
    
     var body: some View {
-        TabView(selection: .constant(1),
-                content:  {
-            Notebook().tabItem {Text("Assignment Notebook")}.tag(1)
-            Settinger().tabItem {Text("Settings")}.tag(2)
-            Text("WIP").tabItem {Text("WIP")}.tag(3)
-                        Text("WIP").tabItem {Text("WIP")}.tag(4)
-        })
+        Text("Homepage")
     }
 }
