8d4ff351fe0db1e90a201b67338bf7487be16f84
Sharul Shah <sharulshah@icloud.com>
2024-05-09 17:56:18 -0500

    Update Homepage.swift

--- Homepage.swift
+++ Homepage.swift
@@ -1,8 +1,28 @@
 import SwiftUI
 
 struct Homepage: View {
-   
+    @State var screenWidth = UIScreen.main.bounds.width
+    @State var screenHeight = UIScreen.main.bounds.height
+    
     var body: some View {
-        Text("Homepage")
+        ZStack {
+            
+              
+            VStack {
+                Text("Home")
+                    .font(Font.custom("SF Compact Rounded", fixedSize: (screenWidth/25)))
+                    .fontWeight(.bold)    
+                
+                Divider()
+                
+                Text("Most Urgent!")
+                    .font(Font.custom("SF Compact Rounded", fixedSize: (screenWidth/25)))
+                    .frame(width: screenWidth, height: 100, alignment: .center)
+                Divider()
+                    .frame(width: 400)
+                
+            }
+           
+        }
     }
 }
