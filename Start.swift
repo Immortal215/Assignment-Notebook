968a15ec7b6f914548cac843922ea82953e2433c
Sharul Shah <sharulshah@icloud.com>
2024-05-13 11:16:17 -0500

    Update and rename start.swift to Start.swift

--- Start.swift
+++ Start.swift
@@ -1,33 +1,31 @@
 import SwiftUI
 
 struct Start: View {
-    @State var starter = true 
+    @State var starter = true
     var body: some View {
         NavigationStack {
             Button {
-                starter = false  
-            } label : {
+                starter = false
+            } label: {
                 ZStack {
                     Image(systemName: "square.and.pencil")
                         .resizable()
-                        .frame(width:200, height:200)
+                        .frame(width: 200, height: 200)
                         .rotationEffect(.degrees(starter ? 0.0 : 15.0))
                         .animation(.bouncy(duration: 1, extraBounce: 0.3))
+                        
+                    Text("The Drawing Board")
+                        .font(.custom("", fixedSize: 100))
+                        .foregroundStyle(.white)
+                        .shadow(color: .gray, radius: 5, x: 0.0, y: 0.0)
                     
-                        Text("The Drawing Board")
-                            .font(.custom("", fixedSize: 100))
-                            .foregroundStyle(.white)
-                            .shadow(color: .gray, radius: 5, x: 0.0, y: 0.0)
-                      
-                   
                 }
                 
             }
             
             Divider()
                 .frame(width: starter ? 0 : .infinity)
-            NavigationLink(destination: Homepage()) { 
-                
+            NavigationLink(destination: ContentView()) {
                 
                 RoundedRectangle(cornerRadius: 20)
                     .foregroundStyle(.blue)
@@ -40,25 +38,15 @@ struct Start: View {
                         Text("Start Planning")
                             .font(.custom("", fixedSize: 50))
                             .foregroundStyle(.white)
-                            .frame(width: starter ? 0 : 500, height: starter ? 0 : 100, alignment:.center)
+                            .frame(width: starter ? 0 : 500, height: starter ? 0 : 100, alignment: .center)
                             .offset(x: 0, y: starter ? -100 : 0)
                             .animation(.bouncy(duration: 1, extraBounce: 0.1))
-                    )   
-                
+                    )
                 
             }
-            .frame(width: starter ? 0 : 500, height: starter ? 0 : 100, alignment:.center)
+            .frame(width: starter ? 0 : 500, height: starter ? 0 : 100, alignment: .center)
             .padding(starter ? 0 : 20)
-
-            
-            
-            
-            
-            
-            
             
         }
-        
-        
     }
 }
