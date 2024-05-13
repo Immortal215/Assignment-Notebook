8017868258ef6f910867372a0aab46fe1eb34f1b
Sharul Shah <sharulshah@icloud.com>
2024-05-13 18:33:35 -0500

    Added Settings

--- Settings.swift
+++ Settings.swift
@@ -1,7 +1,47 @@
 import SwiftUI
 
 struct Settinger: View {
+    
+    @AppStorage("duedatesetter") var dueDateSetter = "One Day" 
+    @State var dueDaters : [String] = ["One Hour", "6 Hours", "One Day", "Two Days"]
+    
+    @AppStorage("organizedAssignments") var organizedAssignments = "Created By Descending (Recent to Oldest)" 
+    @State var organizationOptions : [String] = ["Created By Descending (Recent to Oldest)", "Created By Ascending (Oldest to Recent)"]
+    
     var body: some View {
-        Text("HI")
+        ZStack {
+            VStack {
+                HStack {
+                    Text("Base Due Date Setting")
+                    
+                    Divider()
+                    Picker("",selection: $dueDateSetter) { 
+                        ForEach(dueDaters, id: \.self) {
+                            Text($0)
+                        }
+                    } 
+                    
+                }
+                
+                Divider()
+                
+                HStack {
+                    Text("Assignment Organization Order")
+                    
+                    Divider()
+                    
+                    Picker("" , selection: $organizedAssignments) { 
+                        ForEach(organizationOptions, id: \.self) {
+                            Text($0)
+                        }
+                    } 
+                    
+                }
+                
+            }
+        }
+        
+        
+        
     }
 }
