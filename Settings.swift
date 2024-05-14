4da7f7cf64b43e04575495f30087d26f98e93386
Sharul Shah <sharulshah@icloud.com>
2024-05-14 17:19:38 -0500

    Update Settings.swift

--- Settings.swift
+++ Settings.swift
@@ -12,36 +12,107 @@ struct Settinger: View {
         ZStack {
             VStack {
                 HStack {
-                    Text("Base Due Date Setting")
-                    
-                    Divider()
-                    Picker("",selection: $dueDateSetter) { 
-                        ForEach(dueDaters, id: \.self) {
-                            Text($0)
+                    VStack {
+                        Text("Notebook")
+                        Divider()
+                            .frame(width:100)
+                        List {
+                            HStack {
+                                Text("Base Due Date Setting")
+                                
+                                Divider()
+                                Picker("",selection: $dueDateSetter) { 
+                                    ForEach(dueDaters, id: \.self) {
+                                        Text($0)
+                                    }
+                                } 
+                                .frame(alignment: .trailing)
+                                
+                            }
+                            
+                            
+                            HStack {
+                                Text("Assignment Organization Order")
+                                
+                                Divider()
+                                
+                                Picker("" , selection: $organizedAssignments) { 
+                                    ForEach(organizationOptions, id: \.self) {
+                                        Text($0)
+                                    }
+                                }                            
+                            }
+                            .frame(height:100)
                         }
-                    } 
+                    }
+                    VStack {
+                        Text("WIP1")
+                        Divider()
+                            .frame(width:100)
+                        List {
+                            HStack {
+                                Text("")
+                                
+                                Divider()
+//                                Picker("",selection: $dueDateSetter) { 
+//                                    ForEach(dueDaters, id: \.self) {
+//                                        Text($0)
+//                                    }
+//                                } 
+//                                .frame(alignment: .trailing)
+                                
+                            }
+                            
+                            
+                            HStack {
+                                Text("")
+                                
+                                Divider()
+                                
+//                                Picker("" , selection: $organizedAssignments) { 
+//                                    ForEach(organizationOptions, id: \.self) {
+//                                        Text($0)
+//                                    }
+//                                }                            
+                            }
+                        }
+                    }
+                    VStack {
+                        Text("WIP2")
+                        Divider()
+                            .frame(width:100)
+                        List {
+                            HStack {
+                                Text("")
+                                
+                                Divider()
+//                                Picker("",selection: $dueDateSetter) { 
+//                                    ForEach(dueDaters, id: \.self) {
+//                                        Text($0)
+//                                    }
+//                                } 
+//                                .frame(alignment: .trailing)
+                                
+                            }
+                            
+                            
+                            HStack {
+                                Text("")
+                                
+                                Divider()
+                                
+//                                Picker("" , selection: $organizedAssignments) { 
+//                                    ForEach(organizationOptions, id: \.self) {
+//                                        Text($0)
+//                                    }
+//                                }                            
+                            }
+                            
+                        }
+                    }
                     
                 }
-                
-                Divider()
-                
-                HStack {
-                    Text("Assignment Organization Order")
-                    
-                    Divider()
-                    
-                    Picker("" , selection: $organizedAssignments) { 
-                        ForEach(organizationOptions, id: \.self) {
-                            Text($0)
-                        }
-                    } 
-                    
-                }
-                
             }
-        }
-        
-        
-        
+        }        
     }
 }
