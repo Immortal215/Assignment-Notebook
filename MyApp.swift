fa55e65d82f9934ba14e2d6de6e324dea5e7592f
Sharul Shah <sharulshah@icloud.com>
2024-06-12 22:52:15 -0500

    Update MyApp.swift

--- MyApp.swift
+++ MyApp.swift
@@ -1,10 +1,15 @@
 import SwiftUI
 
 @main
+
 struct MyApp: App {
     var body: some Scene {
         WindowGroup {
             Start()
+                .preferredColorScheme(.dark)
+
         }
+
     }
 }
+
